f6b8b998279b3dce603b8834521132936a4dffa8
shamsTheCoder <sktabrez856@gmail.com>
2020-02-06 20:42:48 +0530

    a

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 const search = document.getElementById('search');
-const match = document.getElementById('match-list');
+const matchList = document.getElementById('match-list');
+
+const searchStates = async searchText => {
+    const res = await fetch('../data/states.json');
+    const states = await res.json();
+
+    //Get matches inputs
+    let matches = states.filter(state => {
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return state.name.match(regex) || state.abbr.match(regex);
+
+    });
+
+    if(searchText.length === 0){
+        matches=[];
+    }
+    
+}
+
+//show result in HTML
 
 
-
-search.addEventListener('input', () => searStates(search.value));
+search.addEventListener('input', () => searchStates(search.value));
